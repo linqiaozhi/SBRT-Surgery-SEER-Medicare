897ba3882e02ca9ca18bd7ddd020b301a5758d0c
apotter1 <apotter1@berkeley.edu>
2023-06-14 14:10:01 -0400

    Added a drug code file

---
+++ DrugCodes.R
@@ -0,0 +1,14 @@
+#Drug Codes of Interest (note that chemotherapy pertains to chemotherapies typically given to lung cancer patients)
+
+#Chemotherapies 
+chemotherapy_all.dxs<-c( )
+carboplatin.dxs<-c()
+cisplatin.dxs<-c()
+
+
+#Anticoagulants 
+anticoagulants_all.dxs<-c()
+heparin.dxs<-c()
+rivaroxban
+
+#Insulins 
