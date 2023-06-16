10a3f3795b8cf6d03d568a0d97fe647b45f987af
linqiaozhi <george.linderman@gmail.com>
2023-06-16 15:39:38 -0400

    restored prior parameters on the nlm function

--- stats/nc_ph.R
+++ stats/nc_ph.R
@@ -18,8 +18,8 @@ cox_nc_est <- function(data, A, X, Z, W, Y, event) {
     sum(colMeans(U_q(tau)) ^ 2)
   }
   tau_init <- coef(glm(A ~ Z + X, family = binomial))
-   nlm_out <- nlm(f = G, p = tau_init, iterlim = 1000, print.level = 2)
-   # nlm_out <- nlm(f = G, p = tau_init, iterlim = 500)
+   # nlm_out <- nlm(f = G, p = tau_init, iterlim = 1000, print.level = 2)
+    nlm_out <- nlm(f = G, p = tau_init, iterlim = 500)
   tau_est <- nlm_out$estimate
   # print(tau_est)
   #  print(sprintf('Number of iterations: %d, cost %f', nlm_out$iter, nlm_out$minimum))
